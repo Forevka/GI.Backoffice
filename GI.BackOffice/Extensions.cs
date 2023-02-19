71858901dadc06bd792ffeb97d363272ec183222
BohdanL <bohdan.lushchyk@no-reply.com>
2023-02-19 22:03:00 +0200

    use custom image converter

--- GI.BackOffice/Extensions.cs
+++ GI.BackOffice/Extensions.cs
@@ -6,20 +6,29 @@ namespace GI.BackOffice
 {
     public static class Extensions
     {
+        public static string CloudFrontUrl = "https://d3w0rb8zugh4tl.cloudfront.net/";
+        //fit-in/filters:quality(80)/filters:format(webp)/media/0zsbssxl/bg-top-4.png
+
         public static IOrderedEnumerable<IPublishedContent> Randomize(this IEnumerable<IPublishedContent> source)
         {
             Random rnd = new();
             return source.OrderBy(item => rnd.Next());
         }
 
-        public static string? GetCropUrlWebp(this MediaWithCrops mediaWithCrops, string cropAlias)
+        public static string? GetCropUrlWebp(this MediaWithCrops mediaWithCrops, string cropAlias, int quality = 80)
         {
-            return mediaWithCrops.GetCropUrl(cropAlias: cropAlias, furtherOptions: "&format=WebP");
+            var cropConfig = mediaWithCrops.LocalCrops.Crops?.FirstOrDefault(x => x.Alias == cropAlias);
+            if (cropConfig != null)
+            {
+                return CloudFrontUrl + $"fit-in/{cropConfig.Width}x{cropConfig.Height}/filters:quality({quality})/filters:format(webp)/" + mediaWithCrops.Url();
+            }
+
+            return GetUrlWebp(mediaWithCrops, quality);
         }
 
-        public static string? GetUrlWebp(this MediaWithCrops mediaWithCrops)
+        public static string? GetUrlWebp(this MediaWithCrops mediaWithCrops, int quality = 80)
         {
-            return mediaWithCrops.Url() + "?format=WebP";
+            return CloudFrontUrl + $"fit-in/filters:quality({quality})/filters:format(webp)/" + mediaWithCrops.Url();
         }
     }
 
